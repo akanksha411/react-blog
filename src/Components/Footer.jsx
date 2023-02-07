c1c3d76b4ce00d219acce2c23ca77cb0488e56dc
Akanksha Tripathi <akankshatripathi411@gmail.com>
2023-02-07 20:32:57 +0530

    committ!!!

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+<div style={{backgroundColor : '#e0dada', textAlign : 'center', padding : '5px', margin : '5px 2px', fontWeight:'bold'}}>&copy; Thanks For visiting our site</div>
+    </>
+  )
+}
+
+export default Footer
